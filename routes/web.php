afd4509d945b35df0467278071ad2eb4521ac1ae
Dirga Yasa <cekingx@gmail.com>
2020-01-23 00:01:08 +0800

    Model Location Membuat migrasi dan controller

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/location', 'LocationController@index')->name('location.index');
+Route::get('/location/create', 'LocationController@create')->name('location.create');
+Route::post('/location/create', 'LocationController@store')->name('location.store');
