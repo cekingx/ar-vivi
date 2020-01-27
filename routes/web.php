7a645ccf703975279c9aa9c0906258c053398a11
Dirga Yasa <cekingx@gmail.com>
2020-01-28 00:42:43 +0800

    SB Admin Template Template ready to use

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dasboard', function() {
+    return view('sb-admin');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
