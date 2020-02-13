b78a065b45637aecaa3e4a0948d5373c94d13c9e
Dirga Yasa <cekingx@gmail.com>
2020-02-13 16:12:54 +0800

    Add cors

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/location', 'LocationController@api');
+Route::middleware('cors')->get('/location', 'LocationController@api');
